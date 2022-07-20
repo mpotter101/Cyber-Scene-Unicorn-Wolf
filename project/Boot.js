df7243825f0f6f72036c187957018f68b927a38a
Michael Potter <michaeljpotter@live.com>
2022-07-19 21:47:31 -0700

    Created basic layout. Ensured Pixi works.

--- project/Boot.js
+++ project/Boot.js
@@ -1,2 +1,9 @@
+// Pixi tutorial: https://github.com/kittykatattack/learningPixi
 // import files needed here
 console.log (PIXI);
+
+PIXI.utils.sayHello();
+
+const app = new PIXI.Application({width: 256, height: 256});
+
+console.log (app);
